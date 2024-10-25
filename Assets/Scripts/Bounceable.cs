935698b579fe414f774bc1d18c628f22c01eb2eb
Akaidus <nikolajjensen6854@gmail.com>
2024-10-25 14:28:14 +0200

    capture device (WIP)

--- Assets/Scripts/Bounceable.cs
+++ Assets/Scripts/Bounceable.cs
@@ -16,7 +16,7 @@ public class Bounceable : MonoBehaviour
     
     [SerializeField] bool isCaptureDevice;
     GameObject capturedObject;
-    bool hasCaptured;
+    bool containsCapture;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -36,62 +36,44 @@ public class Bounceable : MonoBehaviour
         }
     }
 
+    void AttemptCapture(GameObject obj)
+    {
+        if (obj.gameObject.GetComponent<Captureable>() && !containsCapture)
+        {
+            containsCapture = true;
+            capturedObject = obj.gameObject;
+            obj.gameObject.GetComponent<Captureable>().isCaptured = true;
+        }
+        else
+        {
+            print("capture no work");
+        }
+    }
     public void SummonCapture()
     {
         if (capturedObject != null)
         {
-            GameObject summonedCapture = Instantiate(capturedObject);
-            var summonSpeed = 1.5f;
-            summonedCapture.GetComponent<Rigidbody>().velocity = Vector3.forward * summonSpeed;
+            GameObject summonedCapture = Instantiate(capturedObject, transform.position, Quaternion.identity);
+            Destroy(capturedObject);
+            summonedCapture.GetComponent<Captureable>().Summon();
+            containsCapture = false;
             capturedObject = null;
         }
-    }
-
-    IEnumerator ShrinkCapture(float waitTime, GameObject obj)
-    {
-        Vector3 originalSize = new Vector3(1, 1, 1);
-        Vector3 newSize = new Vector3(0, 0, 0);
-        var t = waitTime;
-        //var time = waitTime;
-        while (t > 0)
+        else
         {
-            t -= Time.deltaTime;
-            print("shrink" + t);
-            obj.transform.localScale = new Vector3(t, t, t);
-            if (t < 0)
-                t = 0;
-                //obj.transform.localScale = Vector3.Lerp(originalSize,newSize,t);
+            print("no capture contained");
         }
-        yield return new WaitForSeconds(waitTime-0.1f);
-        //var shrinkVal = 0.1f;
-        //obj.transform.localScale -= new Vector3(shrinkVal * Time.deltaTime,shrinkVal * Time.deltaTime,shrinkVal * Time.deltaTime);
-        //obj.transform.localScale = Vector3.Lerp(originalSize,newSize,waitTime*Time.time);
-        //yield return new WaitForSeconds(waitTime);
     }
+    
     void OnCollisionEnter(Collision collision)
     {
         audioSource.pitch = Random.Range(minPitch, maxPitch);
         audioSource.Play();
-        if (isCaptureDevice)
+        if (isCaptureDevice && capturedObject == null)
         {
-            if (capturedObject == null)
-            {
-                if (collision.gameObject.GetComponent<Captureable>() && !hasCaptured)
-                {
-                    capturedObject = collision.gameObject;
-                    var delay = 1;
-                    StartCoroutine(ShrinkCapture(delay, collision.gameObject));
-                    Destroy(collision.gameObject, delay);
-                    hasCaptured = true;
-                }
-            }
-            Vector3 bounceDirection = Vector3.Reflect(-collision.relativeVelocity, collision.GetContact(0).normal);
-            rb.velocity = bounceDirection * bounceDamping;
+            AttemptCapture(collision.gameObject);
         }
-        else
-        {
-            Vector3 bounceDirection = Vector3.Reflect(-collision.relativeVelocity, collision.GetContact(0).normal);
-            rb.velocity = bounceDirection * bounceDamping;
-        }
+        Vector3 bounceDirection = Vector3.Reflect(-collision.relativeVelocity, collision.GetContact(0).normal);
+        rb.velocity = bounceDirection * bounceDamping;
     }
 }
--- Assets/Scripts/Captureable.cs
+++ Assets/Scripts/Captureable.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Captureable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //[SerializeField] public GameObject prefab;
+    Vector3 originalSize = new(1, 1, 1);
+    Vector3 newSize = new(0, 0, 0);
+    float duration;
+    float multiplier = 1.8f;
+    [HideInInspector] public bool isCaptured;
+    bool isSummoned;
+    new Collider collider;
+
     void Start()
     {
-        
+        collider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isCaptured)
+        {
+            print("capt");
+            Shrink();
+        }
+        if (isSummoned)
+        {
+            print("summ");
+            Expand();
+        }
+    }
+
+    void Shrink()
+    {
+        duration += Time.deltaTime * multiplier;
+        print("shrink" + duration);
+        transform.localScale = Vector3.Lerp(originalSize,newSize, duration);
+        if(transform.localScale == newSize)
+        {
+            collider.enabled = false;
+            isCaptured = false;
+        }
+    }
+
+    void Expand()
+    {
+        isSummoned = true;
+        transform.localScale = Vector3.Lerp(originalSize,newSize, duration);
+        if (transform.localScale == originalSize)
+        {
+            print("full expanded");
+            collider.enabled = true;
+            isSummoned = false;
+        }
+    }
+
+    public void Summon()
+    {
+        transform.localScale = newSize;
+        collider.enabled = true;
+        var delay = 0.1f;
+        Invoke(nameof(Expand), delay);
     }
 }
