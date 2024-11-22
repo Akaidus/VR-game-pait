d5cad3ce18ada9bb1c4a762a786302e74afb5a69
Jakob Winkel Missel <113978150+JakobMissel@users.noreply.github.com>
2024-11-22 20:11:49 +0100

    changes

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,7 @@ public class ChangeScene : MonoBehaviour
 {
     int currentSceneIndex;
 
-    void Start()
+    void Awake()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
@@ -14,6 +14,6 @@ public class ChangeScene : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player") || other.CompareTag("PlayerHand"))
-            SceneManager.LoadScene(currentSceneIndex++);
+            SceneManager.LoadScene(currentSceneIndex+1);
     }
 }
