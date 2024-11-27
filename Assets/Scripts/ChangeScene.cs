b86d630245b0d8c41ad516cf6e571f248957a5bd
Akaidus <nikolajjensen6854@gmail.com>
2024-11-27 17:54:36 +0100

    Update ChangeScene.cs

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,15 +5,20 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     int currentSceneIndex;
+    int totalScenes;
 
     void Awake()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        totalScenes = SceneManager.sceneCountInBuildSettings;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") || other.CompareTag("PlayerHand"))
-            SceneManager.LoadScene(currentSceneIndex+1);
+        if (other.CompareTag("Player") || other.CompareTag("PlayerHand"))
+        {
+            var nextScene = (currentSceneIndex + 1) % totalScenes;
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
